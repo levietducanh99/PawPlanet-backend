515f98aa1383513f7bca3197e2f15a2168168dee
hoan99111 <hoan99111@gmail.com>
2025-12-28 00:32:51 +0700

    Add repository interfaces for encyclopedia and post entities

--- src/main/java/com/pawpplanet/backend/encyclopedia/repository/EncyclopediaSectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pawpplanet.backend.encyclopedia.repository;
-
-import com.pawpplanet.backend.encyclopedia.entity.EncyclopediaSectionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface EncyclopediaSectionRepository extends JpaRepository<EncyclopediaSectionEntity, UUID> {
-}
-
--- src/main/java/com/pawpplanet/backend/notification/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pawpplanet.backend.notification.repository;
-
-import com.pawpplanet.backend.notification.entity.NotificationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
-}
