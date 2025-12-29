9f6cb88c318110e1b7b546c81f391642fffa35b6
levietducanh99 <ducanhvahung@gmail.com>
2025-12-29 17:14:49 +0700

    Refactor FollowUser entity and enhance OpenAPI documentation for media management

--- src/main/java/com/pawpplanet/backend/user/repository/FollowUserRepository.java
+++ src/main/java/com/pawpplanet/backend/user/repository/FollowUserRepository.java
@@ -1,7 +1,9 @@
 package com.pawpplanet.backend.user.repository;
+
 import com.pawpplanet.backend.user.entity.FollowUser;
 import com.pawpplanet.backend.user.entity.FollowUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface FollowUserRepository extends JpaRepository<FollowUser, FollowUserId> {
     int countByIdFollowerId(Long followerId);
     int countByIdFollowingId(Long followingId);
